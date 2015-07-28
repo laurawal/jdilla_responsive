88ba640f534f990ed3acb5dab18e2f5574f0bf52
Laura Walpole <l.a.walpole@googlemail.com>
2015-07-28 15:54:34 +0100

    function for ease of scolling added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//collapse the nav on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 20) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1100, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
